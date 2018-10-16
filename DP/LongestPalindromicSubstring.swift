4a0f0e45d99530dda3cceff21101f6e2a48cf38d
menttofly <1028365614@qq.com>
2018-10-16 23:49:54 +0800

    varify struct

---
+++ DP/LongestPalindromicSubstring.swift
@@ -0,0 +1,9 @@
+//
+//  LongestPalindromicSubstring.swift
+//  LeetCode-Swift
+//
+//  Created by menttofly on 2018/10/16.
+//  Copyright © 2018 menttofly. All rights reserved.
+//
+
+import Foundation
