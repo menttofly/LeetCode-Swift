0fe07836220dfdd675b542fc607ac2c728ad087e
menttofly <1028365614@qq.com>
2018-10-16 23:44:43 +0800

    Initial commit

---
+++ LeetCode-Swift/LeetCode-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeetCode-Swift
+//
+//  Created by menttofly on 2018/10/16.
+//  Copyright © 2018 menttofly. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
