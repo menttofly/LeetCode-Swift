0081a07db7ea65a001c51967df325c0e7df385e9
menttofly <1028365614@qq.com>
2018-10-29 20:38:48 +0800

    First two question

---
+++ Array/TwoSum.swift
@@ -0,0 +1,29 @@
+//
+//  TwoSum.swift
+//  LeetCode-Swift
+//
+//  Created by menttofly on 2018/10/29.
+//  Copyright © 2018年 menttofly. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * Question Link: https://leetcode.com/problems/two-sum/
+ * Primary idea: Traversing the array and store (target - nums[i], i) as (key, value) in a dictionary
+ *
+ * Time Complexity: O(n), Space Complexity: O(n)
+ */
+class TwoSum {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var hashmap: [Int:Int] = [:]
+        for (i, num) in nums.enumerated() {
+            if let index = hashmap[target - num] {
+                return [i, index]
+            } else {
+                hashmap[num] = i
+            }
+        }
+        fatalError("No valid output")
+    }
+}
