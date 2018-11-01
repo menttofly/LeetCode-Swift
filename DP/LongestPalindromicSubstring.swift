d7b84983f6cdff0f3666518aaa76fc3de25a938b
menttofly <1028365614@qq.com>
2018-11-01 20:26:43 +0800

    string question

--- DP/LongestPalindromicSubstring.swift
+++ DP/LongestPalindromicSubstring.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+
+/**
+ * Question Link: https://leetcode.com/problems/longest-palindromic-substring/
+ * Primary idea: Define DP[i][j] to record whether a substring is palindromic or not by Dynamic Programming
+ *
+ * Note: State transition equation - DP(i,j) = (DP(i+1,j−1) and Si==Sj)
+ *
+ * Time Complexity: O(n^2), Space Complexity: O(n^2)
+ */
+class LongestPalindromicSubstring {
+    func longestPalindrome(_ s: String) -> String {
+        let count = s.count
+        if s.isEmpty || count == 1 {
+            return s
+        }
+        var dp = Array(repeating: Array(repeating: false, count: count), count: count)
+        let chars = [Character](s)
+        for i in 0..<count {
+            for j in 0..<count {
+                if i >= j {   /// A string has only one character, or empty is palindromic
+                    dp[i][j] = true
+                }
+            }
+        }
+        var start = 0, end = 0, max = 1
+        for len in 1..<count {
+            for i in 0..<count - len {
+                let j = i + len
+                if chars[i] == chars[j] {
+                    dp[i][j] = dp[i + 1][j - 1]   /// Transfer state to previous one
+                    if dp[i][j] && len + 1 > max {
+                        max = len + 1
+                        start = i
+                        end = j
+                    }
+                }
+            }
+        }
+        return String(chars[start...end])
+    }
+}
---
+++ String/ZigZagConversion.swift
@@ -0,0 +1,77 @@
+//
+//  ZigZagConversion.swift
+//  LeetCode-Swift
+//
+//  Created by menttofly on 2018/11/1.
+//  Copyright © 2018年 menttofly. All rights reserved.
+//
+
+
+import Foundation
+
+/**
+ * Question Link: https://leetcode.com/problems/zigzag-conversion/
+ * Primary idea: Visit the characters by row in the same order as reading the Zig-Zag pattern line by line
+ *
+ * Note: Frequent access to s.count of string is very inefficient in Swift
+ *
+ * Time Complexity: O(n), Space Complexity: O(n)
+ */
+class ZigZagConversion {
+    func convert(_ s: String, _ numRows: Int) -> String {
+        let len = s.count
+        if numRows == 1 || len <= numRows {
+            return s
+        }
+        let chs = [Character](s)
+        var result = [Character]()
+        let period = 2 * numRows - 2   /// Distance between character in the vertical direction
+        for row in 0..<numRows {
+            var index = row
+            while index < len {
+                result.append(chs[index])
+                if row != 0 && row != numRows - 1 {
+                    let distance = period - 2 * row   /// Distance between internal character and its adjacent characters on left
+                    let position = index + distance
+                    if position < len {
+                        result.append(chs[position])
+                    }
+                }
+                index += period
+            }
+        }
+        return String(result)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
