0601cbfc9beba08c667498c4e2e4cb0ff44e6d19
menttofly <1028365614@qq.com>
2018-11-03 15:32:49 +0800

    [Array] Update comment to Two Sum

--- Array/TwoSum.swift
+++ Array/TwoSum.swift
@@ -18,7 +18,7 @@ class TwoSum {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var hash: [Int: Int] = [:]
         for (i, num) in nums.enumerated() {
-            if let index = hash[target - num] {
+            if let index = hash[target - num] {   /// target - num is what we are looking for
                 return [i, index]
             } else {
                 hash[num] = i
