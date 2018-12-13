a9b6960905366d075f762d71a78accd60572b82d
menttofly <1028365614@qq.com>
2018-12-13 23:55:09 +0800

    [LinkedList] Update solution to Reverse Linked List

--- LinkedList/ReverseLinkedList.swift
+++ LinkedList/ReverseLinkedList.swift
@@ -14,7 +14,7 @@ import Foundation
  *
  * Time Complexity: O(n)
  */
-fileprivate class Solution {
+class ReverseLinkedList {
     func reverseList(_ head: ListNode?) -> ListNode? {
         var reversed: ListNode?
         var previous: ListNode?
