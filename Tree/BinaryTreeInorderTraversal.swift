721d105c4c2c8b4070fe15f317a72ddbd1a16684
menttofly <1028365614@qq.com>
2018-12-15 17:08:50 +0800

    [Tree] Add solution to Binary Tree Inorder Traversal

---
+++ Tree/BinaryTreeInorderTraversal.swift
@@ -0,0 +1,32 @@
+//
+//  BinaryTreeInorderTraversal.swift
+//  LeetCode-Swift
+//
+//  Created by menttofly on 2018/12/15.
+//  Copyright © 2018 menttofly. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * Question Link: https://leetcode.com/problems/binary-tree-inorder-traversal/
+ * Primary idea: Classic binary tree traversing.
+ *
+ * Time Complexity: O(n)
+ */
+class BinaryTreeInorderTraversal {
+    func inorderTraversal(_ root: TreeNode?) -> [Int] {
+        var nodes = [Int]()
+        inorderTraversal(root, &nodes)
+        return nodes
+    }
+    
+    func inorderTraversal(_ root: TreeNode?, _ nodes: inout [Int]) {
+        if root == nil {
+            return
+        }
+        inorderTraversal(root?.left, &nodes)
+        nodes.append(root!.val)
+        inorderTraversal(root?.right, &nodes)
+    }
+}
