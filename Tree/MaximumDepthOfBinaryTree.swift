e60db59582db289d3a5ded3995dde68fee011b85
menttofly <1028365614@qq.com>
2018-12-16 19:59:43 +0800

    [Tree] Add solution to Maximum Depth of Binary Tree

---
+++ Tree/MaximumDepthOfBinaryTree.swift
@@ -0,0 +1,26 @@
+//
+//  MaximumDepthOfBinaryTree.swift
+//  LeetCode-Swift
+//
+//  Created by menttofly on 2018/12/16.
+//  Copyright © 2018 menttofly. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * Question Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+ * Primary idea: Recursively iterating over the tree, the depth of node is maxinum of it's two sub-tree depth.
+ *
+ * Time Complexity: O(n), Space Complexity: O(1)
+ */
+class MaximumDepthOfBinaryTree {
+    func maxDepth(_ root: TreeNode?) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        let leftDepth = maxDepth(root.left)
+        let rightDepth = maxDepth(root.right)
+        return max(leftDepth, rightDepth) + 1
+    }
+}
