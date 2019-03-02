d70cb9c9fbe94c718193eb82373999e1e6871f88
menttofly <1028365614@qq.com>
2019-03-03 00:53:30 +0800

    [Array] Add solution to Next Permutation

---
+++ Array/NextPermutation.swift
@@ -0,0 +1,32 @@
+//
+//  NextPermutation.swift
+//  LeetCode-Swift
+//
+//  Created by menttofly on 2019/3/2.
+//  Copyright © 2019 menttofly. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * Question Link: https://leetcode.com/problems/next-permutation/
+ * Primary idea: Find the first decreasing number from low to high in sequence, then swap it with the minimum that large than
+ * this number of right part. At last，reverse the sequence on decreasing number's right part.
+ *
+ * Time Complexity: O(n), Space Complexity: O(1)
+ */
+class NextPermutation {
+    func nextPermutation(_ nums: inout [Int]) {
+        var i = nums.count - 2, j = nums.count - 1
+        while i >= 0 && nums[i] >= nums[i+1] {
+            i -= 1
+        }
+        if i >= 0 {
+            while nums[j] <= nums[i] {
+                j -= 1
+            }
+            nums.swapAt(i, j)
+        }
+        nums[i+1..<nums.count].reverse()
+    }
+}
