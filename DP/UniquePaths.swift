97a88b3b4c11c9fb957618ce87229e744d67c2e6
menttofly <1028365614@qq.com>
2019-03-04 22:03:25 +0800

    [DP] Add solution to Unique Paths

---
+++ DP/UniquePaths.swift
@@ -0,0 +1,29 @@
+//
+//  UniquePaths.swift
+//  LeetCode-Swift
+//
+//  Created by menttofly on 2019/3/4.
+//  Copyright © 2019 menttofly. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * Question Link: https://leetcode.com/problems/unique-paths/
+ * Primary idea: DP(i,j) represent the number of paths to arrive at point (i,j), and robot can only move either down or right.
+ *
+ * State Transition Equation: DP(i,j) = DP(i-1,j) + DP(i,j-1)
+ *
+ * Time Complexity: O(n), Space Complexity: O(n)
+ */
+class UniquePaths {
+    func uniquePaths(_ m: Int, _ n: Int) -> Int {
+        var dp = Array(repeating: Array(repeating: 1, count: n), count: m)
+        for i in 1..<m {
+            for j in 1..<n {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+            }
+        }
+        return dp[m - 1][n - 1]
+    }
+}
