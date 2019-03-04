b537c562c61dc46bb7fb88b18d3e362508edd0d1
menttofly <1028365614@qq.com>
2019-03-04 21:35:11 +0800

    [Array] Add solution to Merge Intervals

---
+++ Array/MergeIntervals.swift
@@ -0,0 +1,52 @@
+//
+//  MergeIntervals.swift
+//  LeetCode-Swift
+//
+//  Created by menttofly on 2019/3/4.
+//  Copyright © 2019 menttofly. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * Question Link: https://leetcode.com/problems/merge-intervals/
+ * Primary idea: Quick sort the intervals by start ascending, then analysis interval overlap.
+ *
+ * Time Complexity: O(nlogn), Space Complexity: O(1)
+ */
+class MergeIntervals {
+    func merge(_ intervals: [Interval]) -> [Interval] {
+        /// Make intervals sorted by Interval.start ascending.
+        let intervals = intervals.sorted { $0.start < $1.start }
+        var results = [Interval]()
+        
+        for interval in intervals {
+            if results.count == 0 {
+                results.append(interval)
+                continue
+            }
+            let previous = results.last!
+            /// If current interval's start larger than previous, it mean no overlapping between this two interval.
+            if interval.start > previous.end {
+                results.append(interval)
+            } else {
+                /// [[1,4],[2,3]] this case.
+                if interval.end > previous.end {
+                    previous.end = interval.end
+                }
+                results[results.count - 1] = previous
+            }
+        }
+        return results
+    }
+}
+
+
+class Interval {
+    var start: Int
+    var end: Int
+    init(_ start: Int, _ end: Int) {
+        self.start = start
+        self.end = end
+    }
+}
