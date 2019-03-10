1139b7491bf37c7556c44961924a57b78f00529d
menttofly <1028365614@qq.com>
2019-03-10 23:57:35 +0800

    [DP] Add solution to Best Time to Buy and Sell Stock with Cooldown

---
+++ DP/BestTimeToBuyAndSellStockWithCooldown.swift
@@ -0,0 +1,28 @@
+//
+//  BestTimeToBuyAndSellStockWithCooldown.swift
+//  LeetCode-Swift
+//
+//  Created by menttofly on 2019/3/10.
+//  Copyright © 2019 menttofly. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * Question Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+ * Primary idea: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/discuss/75927/Share-my-thinking-process
+ *
+ * Time Complexity: O(n), Space Complexity: O(1)
+ */
+class BestTimeToBuyAndSellStockWithCooldown {
+    func maxProfit(_ prices: [Int]) -> Int {
+        var sell = 0, prev_sell = 0, buy = Int.min, prev_buy = 0
+        for price in prices {
+            prev_buy = buy
+            buy = max(prev_sell - price, prev_buy)
+            prev_sell = sell;
+            sell = max(prev_buy + price, prev_sell)
+        }
+        return sell
+    }
+}
