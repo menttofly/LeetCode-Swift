768d8ee54910b0f83ec390162a688da6153609d5
menttofly <1028365614@qq.com>
2019-03-12 22:32:15 +0800

    [Array] Add solution to Move Zeroes

---
+++ Array/MoveZeroes.swift
@@ -0,0 +1,27 @@
+//
+//  MoveZeroes.swift
+//  LeetCode-Swift
+//
+//  Created by menttofly on 2019/3/12.
+//  Copyright © 2019 menttofly. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * Question Link: https://leetcode.com/problems/move-zeroes/
+ * Primary idea: Swap zero from front to back.
+ *
+ * Time Complexity: O(n), Space Complexity: O(1)
+ */
+class MoveZeroes {
+    func moveZeroes(_ nums: inout [Int]) {
+        var r = 0
+        for i in 0..<nums.count {
+            if nums[i] != 0 {
+                nums.swapAt(i, r)
+                r += 1
+            }
+        }
+    }
+}
