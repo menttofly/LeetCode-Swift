7a03b35c6d5a2239536f619c0d740fe0fc9f8541
menttofly <1028365614@qq.com>
2019-03-13 20:39:31 +0800

    [Tree] Add solution to Path Sum

---
+++ Tree/PathSum.swift
@@ -0,0 +1,28 @@
+//
+//  PathSum.swift
+//  LeetCode-Swift
+//
+//  Created by menttofly on 2019/3/13.
+//  Copyright © 2019 menttofly. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * Question Link: https://leetcode.com/problems/path-sum/
+ * Primary idea: Deep-first-seaching and compare the root's value with target sum.
+ *
+ * Time Complexity: O(n^2), Space Complexity: O(n)
+ */
+class PathSum {
+    func hasPathSum(_ root: TreeNode?, _ sum: Int) -> Bool {
+        guard let root = root else {
+            return false
+        }
+        /// Root is a leaf node.
+        if root.left == nil && root.right == nil && root.val == sum {
+            return true
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val)
+    }
+}
