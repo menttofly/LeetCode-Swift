ba45a4bbb52d946475d19fa11658edfbd34fff94
menttofly <1028365614@qq.com>
2021-03-01 14:31:05 +0800

    add coding interview folder

---
+++ Offer/03_数组中重复的数字.swift
@@ -0,0 +1,24 @@
+//
+//  数组中重复的数字.swift
+//  LeetCode-Swift
+//
+//  Created by menttofly on 2021/3/1.
+//  Copyright © 2021 menttofly. All rights reserved.
+//
+
+import Foundation
+
+fileprivate class Solution {
+    func findRepeatNumber(_ nums: [Int]) -> Int {
+        var hash = [Int: Int](), res = 0
+        for num in nums {
+            if let _ = hash[num] {
+                res = num
+                break
+            } else {
+                hash[num] = 1
+            }
+        }
+        return res
+    }
+}
