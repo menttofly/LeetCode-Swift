9745e6c701b2c3ae9d8e9a1cb934d4b0a86b64de
menttofly <1028365614@qq.com>
2021-08-12 14:31:28 +0800

    [Design] Add solution to Next Greater Element II

---
+++ Design/next_greater_element_ii.swift
@@ -0,0 +1,34 @@
+//
+//  next_greater_element_ii.swift
+//  LeetCode-Swift
+//
+//  Created by menttofly on 2021/8/12.
+//  Copyright © 2021 menttofly. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * Question Link: https://leetcode.com/problems/next-greater-element-ii/
+ * Primary idea: 使用单调递减栈对 T 元素索引持续入栈，维护单调递减特性，被弹出的栈的索引的下一更大值就是当前入栈索引对应的元素
+ *
+ * Time Complexity: O(n), Space Complexity: O(n)
+ */
+class NextGreaterElementsII {
+    func nextGreaterElements(_ nums: [Int]) -> [Int] {
+        let size = nums.count
+        var res = Array(repeating: -1, count: size)
+        var stack = [Int]()
+        
+        /// 假装将数组扩容一下，让后面元素可以访问到前面的元素
+        for i in 0..<size * 2 {
+            while !stack.isEmpty && nums[stack.last!] < nums[i % size] {
+                res[stack.removeLast()] = nums[i % size]
+            }
+            
+            stack.append(i % size)
+        }
+        
+        return res
+    }
+}
