a693438aeb342a59c88a25473417a404e5460d58
menttofly <1028365614@qq.com>
2021-08-21 00:14:41 +0800

    [String] Update solution to Find All Anagrams in a String

--- String/find_all_anagrams_in_a_string.swift
+++ String/find_all_anagrams_in_a_string.swift
@@ -15,7 +15,51 @@ import Foundation
  * Time Complexity: O(n), Space Complexity: O(n)
  */
 class FindAllAnagramsInAString {
+    /// 滑动窗口
     func findAnagrams(_ s: String, _ p: String) -> [Int] {
+        let s = [Character](s)
+        var window = [Character: Int]()
+        var target = [Character: Int]()
+        for c in [Character](p) {
+            target[c] = (target[c] ?? 0) + 1
+        }
+        
+        var left = 0, right = 0
+        /// valid 记录已满足条件的字符个数
+        var valid = 0, res = [Int]()
+        
+        while right < s.count {
+            let c = s[right]
+            right += 1
+            
+            if let count = target[c] {
+                window[c, default: 0] += 1
+                /// 同一字符串出现多次时，只有窗口中该字符数等于目标字符串中该字符存在次数，才更新 valid
+                if window[c] == count {
+                    valid += 1
+                }
+            }
+            
+            while right - left >= p.count {
+                if valid == target.count {
+                    res.append(left)
+                }
+                
+                let c = s[left]
+                left += 1
+                if let count = target[c] {
+                    if window[c] == count {
+                        valid -= 1
+                    }
+                    window[c, default: 0] -= 1
+                }
+            }
+        }
+        
+        return res
+    }
+    
+    func findAnagrams1(_ s: String, _ p: String) -> [Int] {
         if s.isEmpty {
             return []
         }
