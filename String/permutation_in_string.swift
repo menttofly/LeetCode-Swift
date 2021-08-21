5a7f753906d3172f6cd9e24e68b5a9f4555024b9
menttofly <1028365614@qq.com>
2021-08-21 14:56:46 +0800

    [String] Update solution to Permutation in String

--- String/permutation_in_string.swift
+++ String/permutation_in_string.swift
@@ -10,47 +10,47 @@ import Foundation
 
 /**
  * Question Link: https://leetcode.com/problems/permutation-in-string/
- * Primary idea: 滑动窗口
+ * Primary idea: 固定窗口大小的滑动窗口
  *
  * Time Complexity: O(n), Space Complexity: O(n)
  */
 class CheckInclusion {
     func checkInclusion(_ s1: String, _ s2: String) -> Bool {
-        let s = [Character](s2)
-        var window = [Character: Int]()
-        var target = [Character: Int]()
-        for c in [Character](s1) {
-            target[c] = (target[c] ?? 0) + 1
+        /// hash 记录字符串 p 中字符及次数
+        var hash = [Character: Int]()
+        for char in [Character](s1) {
+            hash[char, default: 0] += 1
         }
         
-        var left = 0, right = 0
-        /// valid 记录已满足条件的字符个数
-        var valid = 0
+        let s = [Character](s2)
+        /// 左右双指针划分窗口 [left, right)，count 记录未满足的字符个数
+        var left = 0, right = 0, count = s1.count
+        
         while right < s.count {
-            let c = s[right]
+            /// 移动 right 指针，如果 s[right] 中的字符在 hash 中存在次数大等于 1，则 count 计数减 1（表示满足条件字符又增加一个）
+            /// value >= 1 表示当前字符在字符串 p 中存在
+            if let value = hash[s[right]], value >= 1 {
+                count -= 1
+            }
+            hash[s[right], default: 0] -= 1
             right += 1
             
-            if let count = target[c] {
-                window[c, default: 0] += 1
-                /// 同一字符串出现多次时，只有窗口中该字符数等于目标字符串中该字符存在次数，才更新 valid
-                if window[c] == count {
-                    valid += 1
-                }
+            /// 当 count = 0，表示已发现一个 p 的同母异位词子串
+            /// 窗口的 left 指针就是该子串的起始索引
+            if count == 0 {
+                return true
             }
             
-            while right - left >= s1.count {
-                if valid == target.count {
-                    return true
+            /// 当窗口大小和 p 长度一样时，移动 left 指针缩小窗口，以发现下一个匹配项
+            if right - left == s1.count {
+                /// s[left] 字符就在原始的 hash 中，即在 p 中，其次数不会小于 0
+                if let value = hash[s[left]], value >= 0 {
+                    /// 由于将 left 踢出窗口，所以 count 计数需要加 1，表示满足条件字符又减少一个
+                    count += 1
                 }
-                
-                let c = s[left]
+
+                hash[s[left], default: 0] += 1
                 left += 1
-                if let count = target[c] {
-                    if window[c] == count {
-                        valid -= 1
-                    }
-                    window[c, default: 0] -= 1
-                }
             }
         }
         
