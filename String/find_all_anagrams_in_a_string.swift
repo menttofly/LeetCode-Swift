90e72d2a3be9d8a5d99c5536e74e6258cb51a92c
menttofly <1028365614@qq.com>
2021-08-21 14:48:21 +0800

    [String] Update solution to Find All Anagrams in a String

--- String/find_all_anagrams_in_a_string.swift
+++ String/find_all_anagrams_in_a_string.swift
@@ -10,101 +10,52 @@ import Foundation
 
 /**
  * Question Link: https://leetcode.com/problems/find-all-anagrams-in-a-string/
- * Primary idea: Hash table and slide window.
+ * Primary idea: 哈希表记录待满足的字符串，及次数信息，并使用固定窗口大小滑动，及左右指针间距为窗口大小时，left 指针就需要滑动
+ * Note: labuladong 的通用框架适合窗口大小不固定的问题，本题属于固定窗口，所以不用内层循环而是 if 判断窗口大小
  *
  * Time Complexity: O(n), Space Complexity: O(n)
  */
 class FindAllAnagramsInAString {
-    /// 滑动窗口
-    func findAnagrams(_ s: String, _ p: String) -> [Int] {
-        let s = [Character](s)
-        var window = [Character: Int]()
-        var target = [Character: Int]()
-        for c in [Character](p) {
-            target[c] = (target[c] ?? 0) + 1
+    func findAnagrams1(_ s: String, _ p: String) -> [Int] {
+        /// hash 记录字符串 p 中字符及次数
+        var hash = [Character: Int]()
+        for char in [Character](p) {
+            hash[char, default: 0] += 1
         }
         
-        var left = 0, right = 0
-        /// valid 记录已满足条件的字符个数
-        var valid = 0, res = [Int]()
+        let s = [Character](s)
+        /// 左右双指针划分窗口 [left, right)，count 记录未满足的字符个数
+        var left = 0, right = 0, count = p.count
+        var res = [Int]()
         
         while right < s.count {
-            let c = s[right]
+            /// 移动 right 指针，如果 s[right] 中的字符在 hash 中存在次数大等于 1，则 count 计数减 1（表示满足条件字符又增加一个）
+            /// value >= 1 表示当前字符在字符串 p 中存在
+            if let value = hash[s[right]], value >= 1 {
+                count -= 1
+            }
+            hash[s[right], default: 0] -= 1
             right += 1
             
-            if let count = target[c] {
-                window[c, default: 0] += 1
-                /// 同一字符串出现多次时，只有窗口中该字符数等于目标字符串中该字符存在次数，才更新 valid
-                if window[c] == count {
-                    valid += 1
-                }
+            /// 当 count = 0，表示已发现一个 p 的同母异位词子串
+            /// 窗口的 left 指针就是该子串的起始索引
+            if count == 0 {
+                res.append(left)
             }
             
-            while right - left >= p.count {
-                if valid == target.count {
-                    res.append(left)
+            /// 当窗口大小和 p 长度一样时，移动 left 指针缩小窗口，以发现下一个匹配项
+            if right - left == p.count {
+                /// s[left] 字符就在原始的 hash 中，即在 p 中，其次数不会小于 0
+                if let value = hash[s[left]], value >= 0 {
+                    /// 由于将 left 踢出窗口，所以 count 计数需要加 1，表示满足条件字符又减少一个
+                    count += 1
                 }
-                
-                let c = s[left]
+
+                hash[s[left], default: 0] += 1
                 left += 1
-                if let count = target[c] {
-                    if window[c] == count {
-                        valid -= 1
-                    }
-                    window[c, default: 0] -= 1
-                }
             }
         }
         
         return res
-    }
-    
-    func findAnagrams1(_ s: String, _ p: String) -> [Int] {
-        if s.isEmpty {
-            return []
-        }
-        var hash = Array(repeating: 0, count: 256)
-        for letter in p {
-            hash[letter.ascii] += 1
-        }
-        let s = [Character](s), p = [Character](p)
-        var left = 0, right = 0, count = p.count
-        var result = [Int]()
-        
-        while right < s.count {
-            let index = s[right].ascii
-            if hash[index] >= 1 {
-                count -= 1
-            }
-            hash[index] -= 1   /// 字符串p中存在的话对应index哈希值降为0，不存在则小于0
-            right += 1
-            
-            /// When the count is down to 0, means we found the right anagram.
-            /// Then add window's left to result list.
-            if count == 0 {
-                result.append(left)
-            }
-            
-            /// If find the window's size equals to p, then we have to move left forward to find the new match window.
-            if right - left == p.count {
-                let leftIndex = s[left].ascii
-                /// 原属于字符串p中的字符
-                if hash[leftIndex] >= 0 {
-                    count += 1
-                }
-                /// 把left对应的字符踢出窗口，所以重置哈希值
-                hash[leftIndex] += 1
-                left += 1
-            }
-        }
-        return result
-    }
-}
-
-
-fileprivate extension Character {
-    var ascii: Int {
-        let utf8 = String(self).utf8
-        return Int(utf8[utf8.startIndex])
     }
 }
