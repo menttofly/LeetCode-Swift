12be212e258e334d17f308a672fb11f94280ac21
menttofly <1028365614@qq.com>
2021-08-30 23:15:53 +0800

    [DP] Update solution to https://leetcode.com/problems/regular-expression-matching/

--- DP/regular_expression_matching.swift
+++ DP/regular_expression_matching.swift
@@ -10,7 +10,9 @@ import Foundation
 
 /**
  * Question Link: https://leetcode.com/problems/regular-expression-matching/
- * Primary idea: A classic Two-Dimensional Dynamic Programming
+ * Primary idea: 如果 s[0..i) 和 p[0..j) 匹配，则 DP[i][j] = true 否则 false
+ *
+ * Note: https://leetcode.com/problems/regular-expression-matching/discuss/5684/C%2B%2B-O(n)-space-DP
  *
  * Time Complexity: O(n^2), Space Complexity: O(n^2)
  */
@@ -20,21 +22,21 @@ class RegularExpressionMatching {
         if n == 0 {
             return m == 0
         }
-        var chs = [Character](s), chp = [Character](p)
+        let s = [Character](s), p = [Character](p)
         var dp = Array(repeating: Array(repeating: false, count: n + 1), count: m + 1)
         dp[0][0] = true
         
         for i in 0...m {
             for j in 1...n {
-                if chp[j - 1] == "*" {
+                if p[j - 1] == "*" {
                     /// None macth, a* count zero time
                     let none = dp[i][j - 2]
                     /// One or more macth, a* count at least one time
-                    let onemore = (i > 0 && (chs[i - 1] == chp[j - 2] || chp[j - 2] == ".") && dp[i - 1][j])
+                    let onemore = (i > 0 && (s[i - 1] == p[j - 2] || p[j - 2] == ".") && dp[i - 1][j])
                     dp[i][j] = none || onemore
                 } else {
                     /// With no wildcard "*", only if characters on tail matche can transfer state to dp[i - 1][j - 1]
-                    dp[i][j] = i > 0 && (chs[i - 1] == chp[j - 1] || chp[j - 1] == ".") && dp[i - 1][j - 1]
+                    dp[i][j] = i > 0 && (s[i - 1] == p[j - 1] || p[j - 1] == ".") && dp[i - 1][j - 1]
                 }
             }
         }
