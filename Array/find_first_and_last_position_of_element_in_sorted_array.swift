6105a6df50d312175cb203cb7767afb72aa3b272
menttofly <1028365614@qq.com>
2021-09-03 21:31:04 +0800

    [Array] Update solution to Find First and Last Position of Element in Sorted Array

--- Array/find_first_and_last_position_of_element_in_sorted_array.swift
+++ Array/find_first_and_last_position_of_element_in_sorted_array.swift
@@ -10,7 +10,7 @@ import Foundation
 
 /**
  * Question Link: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
- * Primary idea: Find the first number that is greater than or equal to target with binary searching.
+ * Primary idea: 使用二分搜索技巧，在左闭右开区间 [left, right) 中搜索第一个等于 target 或大于的数
  *
  * Time Complexity: O(logn), Space Complexity: O(1)
  */
@@ -20,19 +20,20 @@ class FindFirstAndLastPositionOfElementInSortedArray {
      * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
      */
     func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
-        let leftmost = position(nums, target)
+        let leftMost = position(nums, target)
         
         /// Target too large or
-        if leftmost == nums.count || nums[leftmost] != target {
+        if leftMost == nums.count || nums[leftMost] != target {
             return [-1, -1]
         }
-        let start = leftmost
+        
+        /// 搜索 target + 1，返回的是 target 的 rightMost + 1，减 1 后为 rightMost
         let end = position(nums, target + 1) - 1
-        return [start, end]
+        return [leftMost, end]
     }
     
-    /// Find the first number that is greater than or equal to target.
-    /// It will return nums.count if target larger than nums[nums.count-1].
+    /// 在左闭右开区间 [left, right) 中搜索第一个等于 target 或大于的数
+    /// 如果 target 比所有的数都大，则越界并返回 nums.count
     private func position(_ nums: [Int], _ target: Int) -> Int {
         var left = 0, right = nums.count, mid = 0
         while left < right {
