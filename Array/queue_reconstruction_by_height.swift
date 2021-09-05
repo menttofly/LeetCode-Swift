dfccd16e9d117a88d50de2d977de064003fee6ea
menttofly <1028365614@qq.com>
2021-09-05 21:13:04 +0800

    update array category

--- Array/queue_reconstruction_by_height.swift
+++ Array/queue_reconstruction_by_height.swift
@@ -10,21 +10,23 @@ import Foundation
 
 /**
  * Question Link: https://leetcode.com/problems/queue-reconstruction-by-height/
- * Primary idea: Sort by people's h descending and k ascending, then inset people at k into queue.
+ * Primary idea: 按升高的降序和 k 的升序排序，然后将 people 插入队列中第 k 位置
  *
  * Time Complexity: O(nlogn), Space Complexity: O(n)
  */
 class QueueReconstructionByHeight {
     func reconstructQueue(_ people: [[Int]]) -> [[Int]] {
         let sorted = people.sorted { (left, right) -> Bool in
-            /// Sort by people's h descending and k ascending
+            /// 按升高的降序和 k 的升序排序
             return left[0] > right[0] || (left[0] == right[0] && left[1] < right[1])
         }
+        
         var queue = [[Int]]()
         for people in sorted {
-            /// people[1] is the insert position.
+            /// k 就是插入队列中的位置
             queue.insert(people, at: people[1])
         }
+        
         return queue
     }
 }
