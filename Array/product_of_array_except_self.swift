448c9dc47efd40dd332a8d47fcb38b98f2eb4557
menttofly <1028365614@qq.com>
2021-09-05 20:48:44 +0800

    update array category

--- Array/product_of_array_except_self.swift
+++ Array/product_of_array_except_self.swift
@@ -18,17 +18,18 @@ class ProductOfArrayExceptSelf {
     func productExceptSelf(_ nums: [Int]) -> [Int] {
         var output = Array(repeating: 1, count: nums.count)
         
-        /// ouput_before i
+        /// get ouput_before i
         for i in 1..<nums.count {
             output[i] = output[i - 1] * nums[i - 1]
         }
-        var right = 1, index = nums.count - 1
-        /// output after i
-        while index >= 0 {
-            output[index] *= right
-            right *= nums[index]
-            index -= 1
+        /// right 记录 (i..n] 区间元素的乘积
+        var right = 1
+        /// get output after i
+        for i in stride(from: nums.count - 1, through: 0, by: -1) {
+            output[i] *= right
+            right *= nums[i]
         }
+        
         return output
     }
 }
