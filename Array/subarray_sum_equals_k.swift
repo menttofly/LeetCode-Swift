0ba74f914a5255b559691110871fff07473ac4cd
menttofly <1028365614@qq.com>
2021-09-07 23:24:01 +0800

    update array category

--- Array/subarray_sum_equals_k.swift
+++ Array/subarray_sum_equals_k.swift
@@ -10,7 +10,9 @@ import Foundation
 
 /**
  * Question Link: https://leetcode.com/problems/subarray-sum-equals-k/
- * Primary idea: Prefix sum array, store prefix sum and it's frequency in hash table.
+ * Primary idea: 前缀和数组，记录每个 [0..i] 区间的和。假设区间[left..right] 和为 k，则 sum[right] - sum[left] = k。
+ *
+ *Note: 扫描整个数组，用字典记录每个 sum[left] 的出现次数
  *
  * Time Complexity: O(n), Space Complexity: O(n)
  */
@@ -19,19 +21,22 @@ class SubarraySumEqualsK {
         if nums.count == 0 {
             return 0
         }
-        /// Prefix sum map which record (prefixSum, count) in array.
+        /// 记录数组中的前缀和与出现次数 (prefixSum, count)
         var prefixMap = [Int: Int](), prefixSum = 0, count = 0
         prefixMap[0] = 1
+        
         for num in nums {
+            /// 更新当前的 sum[right]
             prefixSum += num
-            /// If (prefixSum - k) exists, then there is a subarray's sum is k.
+            /// sum[left] 存在且大于 0，则表明 [left..right] 区间和为 k
             if let cnt = prefixMap[prefixSum - k] {
                 count += cnt
             }
-            /// Update prefix sum map.
-            let thiscnt = prefixMap[prefixSum] ?? 0
-            prefixMap[prefixSum] = thiscnt + 1
+            /// 更新当前位置的前缀和出现的次数
+            /// 因为数组元素可能为 0 或者负数，所以同一前缀和可能出现多次，不能粗暴地设置为 1
+            prefixMap[prefixSum, default: 0] += 1
         }
+        
         return count
     }
 }
