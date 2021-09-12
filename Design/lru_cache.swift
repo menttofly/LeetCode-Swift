8b5ca19ba427d633ae721e2755916581a1370dd7
menttofly <1028365614@qq.com>
2021-09-12 23:54:50 +0800

    update design

--- Design/lru_cache.swift
+++ Design/lru_cache.swift
@@ -10,22 +10,23 @@ import Foundation
 
 /**
  * Question Link: https://leetcode.com/problems/lru-cache/
- * Primary idea: Use hash table to visit node, and doubly linked list to implement LRU strategy.
+ * Primary idea: 使用哈希表快速访问元素 node，双向链表实现 LRU 淘汰策略
  *
  * Time Complexity: O(1), Space Complexity: O(n)
  */
 class LRUCache {
     var count: Int = 0
     var table: [Int: LRUNode] = [:]
+    
+    /// 哑节点，方便操作双向链表
+    let head = LRUNode(0)
+    let tail = LRUNode(0)
+    
+    /// 当前缓存容量
     let capacity: Int
-    let head: LRUNode
-    let tail: LRUNode
 
     init(_ capacity: Int) {
         self.capacity = capacity
-        
-        head = LRUNode(0)
-        tail = LRUNode(0)
         head.next = tail
         tail.pre = head
     }
@@ -34,7 +35,7 @@ class LRUCache {
         guard let node = table[key] else {
             return -1
         }
-        /// move
+        /// 标记为最新访问的
         top(node: node)
         return node.val
     }
@@ -42,6 +43,7 @@ class LRUCache {
     func put(_ key: Int, _ value: Int) {
         if let node = table[key] {
             node.val = value
+            /// 标记为最新访问的
             top(node: node)
         } else {
             let node = LRUNode(value)
@@ -50,6 +52,7 @@ class LRUCache {
             insert(node: node)
             count += 1
             
+            /// 超过最大容量，移除队尾元素
             if count > capacity {
                 let last = pop()
                 table.removeValue(forKey: last.key)
@@ -58,7 +61,8 @@ class LRUCache {
         }
     }
     
-    // MARK:- Operate Doubly Linked List
+    // MARK:- 双向链表操作
+    /// 删除某一节点
     private func remove(node: LRUNode) -> Void {
         if let pre = node.pre, let next = node.next {
             pre.next = next
@@ -66,7 +70,7 @@ class LRUCache {
         }
     }
     
-    /// Always insert new node after head.
+    /// 使用头插法始终在哑节点 head 之后插入，作为队头元素
     private func insert(node: LRUNode) -> Void {
         node.pre = head
         node.next = head.next
@@ -75,8 +79,9 @@ class LRUCache {
         head.next = node
     }
     
-    /// Move an existing node to the top if need.
+    /// 将一个节点移动到队头
     private func top(node: LRUNode) -> Void {
+        /// node 是否和 head 的 next 为同一实例
         if head.next! === node {
             return
         }
@@ -84,14 +89,13 @@ class LRUCache {
         insert(node: node)
     }
     
-    /// Pop the last node before tail.
+    /// pop 哑节点 tail 的前一个元素，即队尾元素
     private func pop() -> LRUNode {
         let last = tail.pre!
         remove(node: last)
         return last
     }
 }
-
 
 class LRUNode {
     var val: Int
