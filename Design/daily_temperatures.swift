9d29251851eeb97b9e89d136854ac3b2df524313
menttofly <1028365614@qq.com>
2021-09-13 23:29:36 +0800

    update design category

--- Design/daily_temperatures.swift
+++ Design/daily_temperatures.swift
@@ -10,19 +10,19 @@ import Foundation
 
 /**
  * Question Link: https://leetcode.com/problems/daily-temperatures/
- * Primary idea: 使用单调递减栈对 T 元素索引持续入栈，维护单调递减特性，被弹出的栈的索引的下一更大值就是当前入栈索引对应的温度
+ * Primary idea: 使用单调递减栈对温度 T 的索引持续入栈，维护单调递减特性，被弹出的栈的索引的下一更大值就是当前入栈索引对应的温度
  *
  * Time Complexity: O(n), Space Complexity: O(n)
  */
 class DailyTemperatures {
-    func dailyTemperatures(_ T: [Int]) -> [Int] {
-        var res = Array(repeating: 0, count: T.count)
+    func dailyTemperatures(_ temperatures: [Int]) -> [Int] {
+        var res = Array(repeating: 0, count: temperatures.count)
         var stack = [Int]()
         
         /// 持续对 T 中元素的索引入栈
-        for (i, val) in T.enumerated() {
+        for (i, val) in temperatures.enumerated() {
             /// 维持单调递减特性，将小于 val 的元素对应索引出栈
-            while !stack.isEmpty && T[stack.last!] < val {
+            while !stack.isEmpty && temperatures[stack.last!] < val {
                 /// 出栈的索引下一个更高温度就是 val，两者之间距离为 i - idx
                 let idx = stack.removeLast()
                 res[idx] = i - idx
--- Design/min_stack.swift
+++ Design/min_stack.swift
@@ -10,7 +10,8 @@ import Foundation
 
 /**
  * Question Link: https://leetcode.com/problems/min-stack/
- * Primary idea: If push a value that is less or equal than min, then push the min into stack first so keep previous min always below current min.
+ *
+ * Primary idea: 当 push 一个小等于当前 min 的数时，先再 push 一个 min，然后 push 当前数 x，再更新 min 为 x
  *
  * Time Complexity: O(n), Space Complexity: O(n)
  */
@@ -26,15 +27,19 @@ class MinStack {
     }
     
     func push(_ x: Int) {
+        /// 入栈的新元素会导致 min 变化，则先把旧的 min 值入栈，保持旧的 min 在新的 min 之下
         if x <= min {
             stack.append(min)
             min = x
         }
+        /// 再 push 新值
         stack.append(x)
     }
     
     func pop() {
+        /// 栈顶元素为当前 min，则下面还压着 pre min
         if stack.removeLast() == min {
+            /// 移除 pre min，并更新当前 min 为 pre min
             min = stack.removeLast()
         }
     }
