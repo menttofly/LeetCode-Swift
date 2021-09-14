1d087289b765700c5634d34165cc5b1379656da3
menttofly <1028365614@qq.com>
2021-09-14 23:19:36 +0800

    update course schedule

--- DFS/course_schedule.swift
+++ DFS/course_schedule.swift
@@ -10,17 +10,17 @@ import Foundation
 
 /**
  * Question Link: https://leetcode.com/problems/course-schedule/
- * Primary idea: If there is a ring in the directed graph by DFS, then can's finish all courses.
+ * Primary idea: 通过 dfs 检测课程依赖关系形成的有向图, 如果存在环则无法完成所有课程
  *
  * Time Complexity: O(n), Space Complexity: O(n)
  */
 class CourseSchedule {
     func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
         
-        /// Building graph by adjacency table.
+        /// 通过邻接表构建有向图
         var graph = Array(repeating: [Int](), count: numCourses)
         for pre in prerequisites {
-            /// pre[1] is start in vertex, pre[0] is end.
+            /// pre[1] 是起始顶点，pre[0] 是结束顶点
             var array = graph[pre[1]]
             array.append(pre[0])
             graph[pre[1]] = array
@@ -28,41 +28,42 @@ class CourseSchedule {
         
         /// 0: unvisited, 1: visited, -1: visiting
         var visited = Array(repeating: 0, count: numCourses)
-        /// Traversing the adjacency table
+        /// 遍历图
         for i in 0..<numCourses {
-            /// Find a cycle, then we can't finish all course.
+            /// 如果发现存在环，则无法完成所有课程
             if dfs(&graph, &visited, i) {
                 return false
             }
         }
+        
         return true
     }
     
-    
-    /// Determine if there is a ring in the directed graph that start with node at index.
+    /// 检测有向图中以 index 为起始顶点是否存在环
     ///
     /// - Parameters:
-    ///   - graph: graph represented by adjacency table
-    ///   - visited: mark visit state when doing dfs.
-    ///   - index: adjacency table's index.
-    /// - Returns: whether there is a ring in graph.
+    ///   - graph: 邻接表代表的有向图
+    ///   - visited: 标记顶点访问状态
+    ///   - index: 起始顶点
+    /// - Returns: 有向图中是否存在环
     func dfs(_ graph: inout [[Int]], _ visited: inout [Int], _ index: Int) -> Bool {
-        /// If an node is visiting when dfs to this element, that means there is cycle in directed graph
+        /// 如果一个顶点处于 visiting 状态，再通过 dfs 回到当前节点，代表有向图中存在环
         if visited[index] == -1 {
             return true
         }
-        /// This node already visted.
+        /// 该顶点已被访问过
         if visited[index] == 1 {
             return false
         }
+        /// 标记为正在访问该 index 顶点
         visited[index] = -1
-        /// All adjacent node for node at index.
+        /// 遍历 index 的所有邻接顶点
         for adjacent in graph[index] {
             if dfs(&graph, &visited, adjacent) {
                 return true
             }
         }
-        /// Mark node as visited.
+        /// 标记该顶点已经访问过
         visited[index] = 1
         return false
     }
