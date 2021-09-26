17c149629c99856362f12b0cc91ba6a81c85c2ce
menttofly <1028365614@qq.com>
2021-09-26 17:19:24 +0800

    update dp

--- DP/house_robber.swift
+++ DP/house_robber.swift
@@ -10,7 +10,7 @@ import Foundation
 
 /**
  * Question Link: https://leetcode.com/problems/house-robber/
- * Primary idea: DP[i] means the maximum sum that can be formed by the number of neighbors when reach i position.
+ * Primary idea: DP[i] 代表 nums[0..i] 所有 house 能够打劫的最多钱财
  *
  * State Transition Equation: DP(i) = max{DP(i-1), DP(i-2) + num(i)}
  *
@@ -21,14 +21,18 @@ class HouseRobber {
         if nums.count <= 1 {
             return nums.isEmpty ? 0 : nums[0]
         }
+        
         var dp = Array(repeating: 0, count: nums.count)
+        /// base case
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
+        
         for i in 2..<nums.count {
-            /// dp[i - 1] means don't rob house at i.
-            /// dp[i - 2] + nums[i] rob house at i.
+            /// dp[i-1] 代表不打劫 i 节点
+            /// dp[i-2] + nums[i] 代表打劫 i 节点
             dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
         }
+        
         return dp[nums.count - 1]
     }
 }
