fc184dc1b5a0e7dbc8c8bada5e76b8509d3d08ad
menttofly <1028365614@qq.com>
2021-10-03 12:44:13 +0800

    update linkedlist category

--- LinkedList/reverse_linked_list_ii.swift
+++ LinkedList/reverse_linked_list_ii.swift
@@ -10,35 +10,37 @@ import Foundation
 
 /**
  * Question Link: https://leetcode.com/problems/reverse-linked-list-ii/
- * Primary idea: 先实现反转前n个节点，然后确定问题base case，即left = 1时就是reverseFront
+ * Primary idea: 先实现反转前 n 个节点，然后确定问题 base case，即 left = 1 时就是 reverseFront
  *
  * Time Complexity: O(n), Space Complexity: O(n)
  */
 
 class ReverseLinkedListII {
+    /// 后继节点
     var successor: ListNode?
-    
-    /// 反转链表的前num个节点，并返回翻转后的头节点
-    func reverseFront(_ head: ListNode?, _ num: Int) -> ListNode? {
-        if num == 1 {
-            successor = head?.next
-            return head
-        }
-        
-        let rhead = reverseFront(head?.next, num - 1)
-        head?.next?.next = head ?? nil
-        head?.next = successor ?? nil
-        
-        return rhead
-    }
     
     /// 反转链表的left到right之间的节点，并返回翻转后的头节点
     func reverseBetween(_ head: ListNode?, _ left: Int, _ right: Int) -> ListNode? {
         if left == 1 {
             return reverseFront(head, right)
         }
-        
+        /// 从 head?.next 开始翻转，返回翻转后头节点，拼接到当前 head 之后
         head?.next = reverseBetween(head?.next, left - 1, right - 1)
         return head
+    }
+    
+    /// 反转链表的前 n 个节点，并返回翻转后的头节点
+    func reverseFront(_ head: ListNode?, _ n: Int) -> ListNode? {
+        if n == 1 {
+            /// 记录第 n + 1 个节点
+            successor = head?.next
+            return head
+        }
+        
+        let rhead = reverseFront(head?.next, n - 1)
+        head?.next?.next = head
+        head?.next = successor
+        
+        return rhead
     }
 }
--- LinkedList/reverse_nodes_in_k_group.swift
+++ LinkedList/reverse_nodes_in_k_group.swift
@@ -16,11 +16,12 @@ import Foundation
  */
 
 class ReverseNodesInKGroup {
-    /// [a, b)
+    /// 翻转区间 [head, tail) 内的元素
     func reverseBetween(_ head: ListNode?, _ tail: ListNode?) -> ListNode? {
-        var pre: ListNode?, cur = head, rear = head
+        var pre: ListNode?, cur = head
+        
         while cur !== tail {
-            rear = rear?.next
+            let rear = cur?.next
             cur?.next = pre
             pre = cur
             cur = rear
@@ -36,11 +37,14 @@ class ReverseNodesInKGroup {
         
         var p: ListNode? = head
         for _ in 0..<k {
+            /// base case，不足 k 个 无需翻转
             if p == nil { return head }
+            /// 让 p 前进到 k + 1 的位置，即下一组
             p = p?.next
         }
-        
+        /// 翻转 [head, p) 之间的元素
         let rhead = reverseBetween(head, p)
+        /// 从 p 开始 k 个一组翻转链表，返回 reversed head，作为 head 节点的后继
         head.next = reverseKGroup(p, k)
         
         return rhead
